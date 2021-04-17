0270ed2b51b2861f1d5e49f1b61ac94439f651a3
bruno edoh <edohbruno@gmail.com>
2021-04-17 22:10:49 +0000

    rn++

--- App.tsx
+++ App.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, ScrollView, Platform } from "react-native";
+import {  TextInput } from "react-native-gesture-handler";
+// const Hello = React.lazy(() => import("./"))
 
 export default function App() {
+  const [sec, setSec] = React.useState(0)
+  const r = React.useRef();
+
+  React.useEffect(() => {
+    console.log("REF:: ", r);
+    console.log("hello")
+
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ScrollView style={styles.container}> 
+      <TextInput style={styles.input} />
+      <Text ref={r}>seconds:: {console.log("platform ", Platform.Version)}</Text>
+      <React.Suspense fallback={<Text>loading...</Text>}>
+
+      </React.Suspense>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff', 
+    backgroundColor: "#fff",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
---
+++ src/components/Hello.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Text } from "react-native";
+
+export default () => {
+  return <Text>Hello</Text>;
+};
